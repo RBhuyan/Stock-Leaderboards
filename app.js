408a0ceb4d811dce28a0653ea59739de34e47b11
RohanBhuyan <rbhuyan@its.jnj.com>
2019-12-04 13:58:15 -0500

    set up mongodb and some initial npm packages

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose')
 const port = 4000
+
+mongoose.connect("mongodb://localhost:27017/sfe_asset", {useNewUrlParser: true});
 
 app.get('/', (req,res) => res.send('Hello World'))
 
